61a92a58d33310202c3730a0896809d256cb3066
Aung Moe Myint Thu <aungmoemyintthu@gmail.com>
2024-03-04 17:34:00 +0630

    update ui

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -6,30 +6,22 @@ import { ref } from 'vue'
 const currentTab = ref('About')
 
 const tabs = {
-  About,
-  Contact
+    About,
+    Contact
 }
 </script>
-
 <template>
-  <div class="flex flex-col rounded-md bg-primary overflow-hidden">
-    <div class="flex bg-[#4D565F] pt-1 rounded-t-md text-[11px]">
-      <div class="w-10 px-1 flex justify-around items-center">
-        <div class="rounded-full w-2 h-2 bg-red-500"></div>
-        <div class="rounded-full w-2 h-2 bg-yellow-500"></div>
-        <div class="rounded-full w-2 h-2 bg-green-500"></div>
-      </div>
-      <button
-        v-for="(_, tab) in tabs"
-        :key="tab"
-        class="transition-all pr-10 py-2 hover:bg-gray-500 rounded-t-[5px] px-2"
-        :class="{ 'bg-primary  hover:bg-primary ': currentTab === tab }"
-        @click="currentTab = tab"
-      >
-        {{ tab + '.json' }}
-      </button>
+    <div class="flex flex-col rounded-md bg-primary overflow-hidden">
+        <div class="flex bg-[#4D565F] pt-1 rounded-t-md text-[11px]">
+            <div class=" px-2 flex justify-around items-center gap-1">
+                <div class="rounded-full w-2 h-2 bg-red-500"></div>
+                <div class="rounded-full w-2 h-2 bg-yellow-500"></div>
+                <div class="rounded-full w-2 h-2 bg-green-500"></div>
+            </div>
+            <button v-for="(_, tab) in tabs" :key="tab" class="transition-all pr-10 py-[7px] hover:bg-gray-500 rounded-t-[5px] px-2" :class="{ 'bg-primary  hover:bg-primary ': currentTab === tab }" @click="currentTab = tab">
+                {{ tab + '.json' }}
+            </button>
+        </div>
+        <component :is="tabs[currentTab]" class="w-full h-full p-4 font-custom"></component>
     </div>
-
-    <component :is="tabs[currentTab]" class="w-full h-full p-4 font-custom"></component>
-  </div>
 </template>
